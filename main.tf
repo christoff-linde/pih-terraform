4c329a60790abbb8c62e080070b9281eab3e96b1
christoff-linde <christoff.linde@gmail.com>
2023-11-12 22:36:58 +0200

    feat: add intial iot-core thing

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
 provider "aws" {
-  # Configuration options
+  region = var.region
 }
+
+resource "aws_iot_thing" "esp32" {
+  name = "esp32-thing"
+}
+
+resource "aws_iot_certificate" "iot_certificate" {
+  active = true
+}
+
+resource "aws_iot_thing_principal_attachment" "iot_attachment" {
+  principal = aws_iot_certificate.iot_certificate.arn
+  thing     = aws_iot_thing.esp32.name
+}
+
