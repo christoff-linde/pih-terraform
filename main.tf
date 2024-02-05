ca1e2691dce323e5ef22b9fbd215d3f369678433
christoff-linde <christoff.linde@gmail.com>
2024-02-05 17:01:57 +0200

    feat(iot-core): add initial iot core configs & variables
    This still needs a lot of work. Currently, no actual rules are setup, for e.g.

--- main.tf
+++ main.tf
@@ -2,8 +2,14 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_iot_thing" "esp32" {
-  name = "esp32-thing"
+resource "aws_iot_thing" "esp32_thing" {
+  for_each        = toset(var.esp32_conditions_device_id_list)
+  name            = "${var.esp32_conditions_device_group}-${each.value}"
+  thing_type_name = aws_iot_thing_type.esp32_conditions_device.name
+}
+
+resource "aws_iot_thing_type" "esp32_conditions_device" {
+  name = "esp32_conditions_device"
 }
 
 resource "aws_iot_certificate" "iot_certificate" {
--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "aws_region" {
   description = "AWS target region"
   default     = "eu-west-1"
 }
+
+variable "esp32_conditions_device_id_list" {
+  type        = list(string)
+  description = "The SENSOR_ID value for each esp32 device"
+  default     = ["01", "02", "03"]
+}
+
+variable "esp32_conditions_device_group" {
+  type        = string
+  description = "Group for esp32 conditions devices"
+  default     = "esp32_conditions_device_group"
+}
